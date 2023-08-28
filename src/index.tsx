afa023af1ac6e48d4a5e43c8fc4ad00c4c62e349
Nastya <voronkina-nastena@mail.ru>
2023-08-28 20:50:11 +0300

    Fix gh-pages routing

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename='/free-to-play-games'>
     <Provider store={store}>
       < App />
     </Provider >
